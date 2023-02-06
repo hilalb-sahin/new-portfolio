d74342a27b8f7aece145da738eec4380fa13d417
Hilal <hilal@Hilals-MacBook-Air.local>
2023-02-06 12:52:08 +0000

    created data.js for project details and changed script.js for it to use data.js

---
+++ data.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        "name": "React Monster Search",
+        "description": "Search a monster. Fetched monster details from JSON object. Valued modularity when designing.",
+        "img-path": "./images/monster.gif",
+        "website-link": "https://hilalb-sahin.github.io/monster-react/"
+    },
+    {
+        "name": "E-commerce website w Firebase",
+        "description": "A website with a shopping cart, uses Firebase to store data",
+        "img-path": "./images/ecommerce.png",
+        "website-link": "https://hilalb-sahin.github.io/clothing-web/"
+    },
+    {
+        "name": "Editable table with React",
+        "description": "A table with sortable and editable columns, includes a search bar",
+        "img-path": "./images/table.png",
+        "website-link": "https://hilalb-sahin.github.io/react-table/"
+    },
+    {
+        "name": "Interactive Website",
+        "description": "Listens events, adds comment to the box using HTML forms, includes a hidden element",
+        "img-path": "./images/interactive.gif",
+        "website-link": "https://hilalb-sahin.github.io/interactive-web/"
+    },
+    {
+        "name": "React Animal Facts",
+        "description": "Update the website with React after clicking on animals",
+        "img-path": "./images/animal.gif",
+        "website-link": "https://hilalb-sahin.github.io/react-demo/"
+    },
+    {
+        "name": "Restaurant Website",
+        "description": "A website with menu and clickable elements, includes CSS slideshow",
+        "img-path": "./images/restaurant.gif",
+        "website-link": "https://hilalb-sahin.github.io/june-restaurant/"
+    },
+    {
+        "name": "Movie Data Sorter",
+        "description": "Fetch data from a javascript object and display it in HTML table. Sort the data in ascending order by clicking the header.",
+        "img-path": "./images/movie_data.gif",
+        "website-link": "https://hilalb-sahin.github.io/movie-sorter/"
+    },
+
+]
+
+export default projects;
+
+
--- script.js
+++ script.js
@@ -1,40 +1,87 @@
-infoBtn = document.querySelectorAll('.info');
-targetDiv = document.querySelectorAll('.projectDetail');
+import  projects  from './data.js';
+
+let projectsContainer = document.getElementById("projects-container");
+
+let projectDetailDiv = document.createElement("div");
+projectDetailDiv.classList.add("projectDetail");
+
+for (let idx = 0; idx < projects.length; idx++) {
+
+
+  //creating the necessary elements
+  let div = document.createElement("div");
+  div.id = "div";
+  let nameP = document.createElement("p");
+  nameP.id = "projectName";
+  nameP.innerHTML = projects[idx].name;
+  
+  let projectDiv = document.createElement("div");
+  projectDiv.id = "project";
+  
+  let img = document.createElement("img");
+  img.id = "project-img";
+  img.src = projects[idx]['img-path'];
+  
+  let buttonsDiv = document.createElement("div");
+  buttonsDiv.id = "buttons";
+  
+  
+  let websiteBtn = document.createElement("a");
+  websiteBtn.classList.add("bubbly-button");
+  websiteBtn.id = "website-anchor";
+  websiteBtn.target = "_blank";
+  websiteBtn.innerHTML= "Website";
+  websiteBtn.href = projects[idx]['website-link'];
+  
+  let projectDetailDiv = document.createElement("div");
+  projectDetailDiv.classList.add("projectDetail");
+  
+  let descriptionP = document.createElement("p");
+  descriptionP.id = "description";
+  descriptionP.innerHTML = projects[idx].description;
+
+  //appending the elements
+  
+  div.appendChild(nameP);
+  div.appendChild(projectDiv);
+  projectDiv.appendChild(img);
+  projectDiv.appendChild(buttonsDiv);
+  buttonsDiv.appendChild(websiteBtn);
+  projectDiv.appendChild(projectDetailDiv);
+  projectDetailDiv.appendChild(descriptionP);
+  projectsContainer.appendChild(div);
+
+
+}
 
 
 
 
 
-      for(let i =0 ; i<targetDiv.length ; i++)
-      { 
         
-        infoBtn[i].addEventListener("click", function() {
-          if(targetDiv[i].style.display == 'block'){
-            targetDiv[i].style.display = 'none';
-          }
-          else{
-            targetDiv[i].style.display = 'block';
+//for each project
 
-            }
-          }
-          );
-          
-        }
 
-var animateButton = function(e) {
 
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
+
+/* 
+<div id="container">
+  <div id="div">
+      <p id="projectName">Movie Data Sorter</p>
+      <div id="project">
+          <img id="project-img" src="images/movie_data.gif">
+
+          <div id="buttons">
+              <button class="info bubbly-button">Info</button>
+              <a class="bubbly-button" id="website-anchor" target="_blank"
+                  href="https://hilalb-sahin.github.io/movie-sorter/">Website</a>
+          </div>
+
+          <div class="projectDetail">
+              <p id="description"> </p>
+
+          </div>
+      </div>
+  </div>
+
+</div> */
